f017ea0163f71c1debdb2c239d09a5dc670f4a79
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-27 18:32:52 +0900

    te

--- oktkit.js
+++ oktkit.js
@@ -11,19 +11,8 @@ async function makeComment() {
       return;
     }
     const pull_request_number = context.payload.pull_request.number;
-    const myToken = core.getInput('myToken');
-    const octokit = github.getOctokit(myToken);
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
-
-    console.log(!!process.env.GITHUB_TOKEN)
-    console.log(!!myToken)
-    core.info(octokit)
-    core.info(octokit.rest)
-
-    core.info(octokit.context);
-    core.info(octokit.info)
-    core.info(octokit.issues)
-    core.info(pull_request_number);
 
     await octokit.rest.issues.createComment({
       ...context.repo,
