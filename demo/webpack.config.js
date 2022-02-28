0be46eb59ec012ea66f0f086eb1deaf339cc6ef7
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-28 18:59:30 +0900

    main push

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,7 +1,15 @@
+
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
 module.exports = {
   mode: 'production',
   entry: './main.js',
   output: {
     filename: 'bundle.js'
-  }
+  },
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static',
+    })
+  ]
 };
