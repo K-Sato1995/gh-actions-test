dea714cbf017df20aa74665a0e3a47e30b87b1a1
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-28 20:16:25 +0900

    main push

--- bin/mainpush.sh
+++ bin/mainpush.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 cd /Users/katsuki/WorkSpace/TypeScript/gh-actions-test
-rm -rf dist
-npm run prepare && git add . && git commit -m 'main push' && git push origin main
+git add . && git commit -m 'main push' && git push origin main
