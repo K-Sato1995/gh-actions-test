26bbfb5b0aea789027add5924df84e826a044232
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-28 18:17:11 +0900

    Add example analyzer to demo

---
+++ demo/analyze.js
@@ -0,0 +1,38 @@
+process.env.NODE_ENV = 'production'
+
+const webpack = require('webpack')
+
+const analyze = () => {
+  try {
+    const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+      .BundleAnalyzerPlugin
+
+    const webpackConfigProd = require('./webpack.config')
+
+    if (!webpackConfigProd.plugins) {
+      webpackConfigProd["plugins"] = []
+    }
+
+    // pushing BundleAnalyzerPlugin to plugins array
+    webpackConfigProd.plugins.push(
+      new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
+    )
+    // actually running compilation and waiting for plugin to start explorer
+    const compiler = webpack(webpackConfigProd)
+    compiler.run((err, stats) => { // [Stats Object](#stats-object)
+      // console.log(stats)
+
+      if (err || stats.hasErrors()) {
+        console.log(err)
+      }
+      compiler.close((closeErr) => {
+        console.log(closeErr)
+      });
+    });
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+analyze()
