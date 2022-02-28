9903d010e8ca4f2a87573ec6afd7d6cf4490cbf2
Katsuki <Katsuki_Sato0130@icloud.com>
2022-02-28 17:35:24 +0900

    tmp

---
+++ demo/index.js
@@ -0,0 +1,8 @@
+
+
+function main() {
+  console.log('Hi')
+  console.log(`Let's increase size a little bit :)`)
+}
+
+main()
---
+++ demo/webpack.config.js
@@ -0,0 +1,19 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+const path = require('path')
+
+module.exports = function () {
+  return {
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+      path: path.resolve(__dirname, 'demo-dist'),
+      filename: './index.js'
+    },
+    plugins: [
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'disabled',
+        generateStatsFile: true
+      })
+    ]
+  }
+}
+
