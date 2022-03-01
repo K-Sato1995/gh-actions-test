12f200b1f5628d1fbbd2f1f69d1bb78283bf1b9f
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-01 10:20:59 +0900

    main push

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');
 // const makeComment = require('./oktkit')
 const analyze = require('./analyze');
-const upload = require('./upload');
-const deployPages = require('./deploy')
+// const upload = require('./upload');
+// const deployPages = require('./deploy')
 
 // most @actions toolkit packages have async methods
 async function run() {
@@ -12,8 +12,8 @@ async function run() {
     core.info(`Waiting ${ms} milliseconds ...`);
     core.info(`HELLO ${name}`)
     await analyze()
-    await deployPages()
-    await upload()
+    // await deployPages()
+    // await upload()
     // await makeComment()
   } catch (error) {
     core.setFailed(error.message);
