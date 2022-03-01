a0562eec250cf8b7d1f3b98c097c6c6055f158ef
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-01 11:51:41 +0900

    main push

--- analyze.js
+++ analyze.js
@@ -31,7 +31,7 @@ const analyze = async () => {
 
     // pushing BundleAnalyzerPlugin to plugins array
     webpackConfigProd.plugins.push(
-      new BundleAnalyzerPlugin({ analyzerMode: 'static', reportFilename: `${process.env.GITHUB_WORKSPACE}/analyzeOutput/index.html` }),
+      new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false, reportFilename: `${process.env.GITHUB_WORKSPACE}/analyzeOutput/index.html` }),
     )
 
     // actually running compilation and waiting for plugin to start explorer
